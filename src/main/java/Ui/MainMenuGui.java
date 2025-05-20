0199fdd442fcde284e84fc7f864f84140cc6e90b
jesidsantiago <jesidsantiago90@gmail.com>
2025-05-20 12:42:36 -0500

    se agrega el main de la interfaz grafica

---
+++ src/main/java/Ui/MainMenuGui.java
@@ -0,0 +1,22 @@
+package Ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class MainMenuGui {
+    private JPanel PanelPrincipal;
+    private JLabel eventMasterProMenuLabel;
+    private JButton exitButton;
+    private JButton manageAccessControlButton;
+    private JButton manageTicketButton;
+    private JButton manageFinancialsButton;
+    private JButton manageVenueButton;
+    private JButton manageArtistButton;
+    private JButton manageEventButton;
+    private JPanel JpanelTituloPrincipal;
+    private JPanel JPanelMenuEventMasterPro;
+}
+
+
+
+
